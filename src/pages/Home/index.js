b22332e14aa34c6e016874a78f005c2ca7e9819f
root <root@DESKTOP-S3HQ9BN.localdomain>
2023-09-20 17:19:10 -0300

    Adicionado carregamento de filmes com mais velocidade e trabalhando na página de erro

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import "./home.css";
 function Home(){
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function loadFilmes(){
@@ -17,12 +18,21 @@ function Home(){
 
             //console.log(response.data.results.slice(0,10));
             setFilmes(response.data.results.slice(0,10));
+            setLoading(false);
         }
 
         loadFilmes();
 
     },[]);
 
+    if(loading){
+        return(
+            <div className="loading">
+                <h2>Carregando filmes...</h2>
+            </div>
+        );
+    }
+    
     return(
         <div className="container">
             <div className="lista-filmes">
